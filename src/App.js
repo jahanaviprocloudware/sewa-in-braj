566e5e611899ec943cc1cb15b7ea8ea7e3dddf8f
jahanavi <jahanaviprocloudware@gmail.com>
2025-07-02 18:09:08 +0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { Provider } from 'react-redux';
-import { Home } from './sewaInBraj/Home/Home';
-import {NavigatedPages} from './sewaInBraj/NavigatedPages/NavigatedPages.jsx';
-import { store } from './sewaInBraj/app/store/responseStore';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ProductInfo } from './sewaInBraj/ProductInfo/ProductInfo.jsx';
-import { Shop } from './Shoping/Shop.jsx';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './sewaInBraj/app/store/responseStore';
+import { NavigatedPages } from './sewaInBraj/NavigatedPages/NavigatedPages.jsx';
 
 function App() {
   return (
